f86173d98cd2a4a6be1ff2b68f14a3da93616ab8
Cloud Dark <clouddark1798@gmail.com>
2023-02-19 16:49:30 +0700

    Update DefaultResumeData.jsx

--- components/utility/DefaultResumeData.jsx
+++ components/utility/DefaultResumeData.jsx
@@ -1,59 +1,40 @@
 const DefaultResumeData = {
-  name: "MARCUS HALL",
+  name: "Mas Syahdan Filsafan",
   position: "Developer",
-  contactInformation: "+1-555-0100",
-  email: "beddylea@gmail.com",
-  address: "San Francisco, CA",
+  contactInformation: "+62899 8937 095",
+  email: "syahdanfilsafan58@gmail.com",
+  address: "Jakarta, Indonesua",
   profilePicture: "",
   socialMedia: [
     {
       socialMedia: "Github",
-      link: "github.com//bedivere-lea",
+      link: "github.com/Cloud-Dark",
     },
     {
       socialMedia: "LinkedIn",
-      link: "linkedin.com/in/bedivere-lea",
+      link: "linkedin.com/in/mas-syahdan-filsafan",
     },
     {
       socialMedia: "Website",
-      link: "bedivere-lea.github.io",
+      link: "syahdandev.blogspot.com",
     },
   ],
-  summary: "Resourceful Developer with 11 years of experience in designing and developing user interfaces, testing and training employees. Skilled at utilizing a wide variety of tools and programs to provide effective applications.",
+  summary: "I am graduated from S1 Information System in University NU Surabaya specializing in Web Developer (Backend), and Certified as an Intermediate Auditor and Program Analyst. have experience on many kind Web Developer, I have Great Understanding about Web Developer and troubleshooting system. I also have the ability to public speaking and have achievement in a presentation competition where I studied in college. Willing to learn new technology and collaboration with each other with friendliness and respectfulness.",
   education: [
     {
-      "school": "Bachelor of Computer Science",
-      "degree": "New York University",
-      "startYear": "2020-08-20",
+      "school": "Institut Teknologi Sepuluh Nopember (ITS)",
+      "degree": "Master's degree",
+      "startYear": "2022-08-20",
       "endYear": "2024-07-01"
     },
-  ],
-  workExperience: [
     {
-      "company": "Torph TTC",
-      "position": "Developer",
-      "description": "Torph TTC is a global software company that offers user interface UI development tools and components for a range of developer applications across all platforms.",
-      "keyAchievements": "Created and maintained 10 web applications for numerous national and foreign clients.\nEnsured that the user interfaces and user experience of the software applications developed by the team met at least 80% of users expectations.\nCreated and analyzed 500 unit test cases.\nDeveloped python scripts to automate image's noise-reduction process which helped improve research analysis time by 40%.\nEstablished and lead a team of 10 people; covering every key role in the early stages.",
-      "startYear": "2023-02-16",
-      "endYear": "2023-02-21"
+      "school": "Universitas Nahdlatul Ulama Surabaya",
+      "degree": "Bachelor of Computer Science",
+      "startYear": "2016-08-20",
+      "endYear": "2020-07-01"
     },
-    {
-      "company": "Reilty Group",
-      "position": "Front End Web Developer",
-      "description": "Reilty Group is an industry-leading provider of online gambling software and solutions.",
-      "keyAchievements": "Increased by 35% the reach of users to the platform, over the installation of the web platform in mobile devices.\nDelivered 30 web solutions.\nReplaced 1 full-time web developer under part-time employment achieving a productivity gain of over 60% as measured by working hours.\nWorked with more than 4 different teams at different points of time and provided end-to-end solutions for clients.",
-      "startYear": "2023-02-22",
-      "endYear": "2023-03-09"
-    },
-    {
-      "company": "HellX",
-      "position": "Python Developer",
-      "description": "Heller is a Software Editor specialized in logistics management.",
-      "keyAchievements": "Developed several automation systems - saved over $50,000 annually.\nJoined the team as #10 employee as a developer working directly with the biggest customers.\nInvolved in coding as per the requirement, delivered well ahead of time which saved project cost by 20%.\nReduced database size by nearly 50% and improved its performance by 20%.\nDelivery time of good reduced by 50%.",
-      "startYear": "2023-02-22",
-      "endYear": "2023-03-09"
-    }
   ],
+  workExperience: [],
   projects: [],
 
   technicalSkills: [
